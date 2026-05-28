15407985d13f888d84c8d9bfd12e28c74defed36
user <guilhermevasconcelosdev@gmail.com>
2026-05-28 19:44:46 -0300

    feat: remove feedback

--- src/main/java/br/com/projetofaculdade/sistema_biblioteca/database/repository/iLivroRepository.java
+++ src/main/java/br/com/projetofaculdade/sistema_biblioteca/database/repository/iLivroRepository.java
@@ -8,7 +8,7 @@ import br.com.projetofaculdade.sistema_biblioteca.database.model.LivrosEntity;
 
 public interface iLivroRepository extends JpaRepository<LivrosEntity, Integer> {
 
-    //Busca livros ignorando maiúsculas e minúsculas e pegando partes da palavra
+    
     List<LivrosEntity> findByNomeContainingIgnoreCase(String nome);
 
     List<LivrosEntity> findByAutorId(Integer autorId);
