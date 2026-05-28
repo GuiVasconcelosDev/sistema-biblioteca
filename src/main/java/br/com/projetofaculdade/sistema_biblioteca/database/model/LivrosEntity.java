f0b38dcef6b042a04a3481fb27089bbe55e85eff
user <guilhermevasconcelosdev@gmail.com>
2026-05-28 14:59:40 -0300

    feat: ajueste e add de novas atribuições e lógico de serviço

--- src/main/java/br/com/projetofaculdade/sistema_biblioteca/database/model/LivrosEntity.java
+++ src/main/java/br/com/projetofaculdade/sistema_biblioteca/database/model/LivrosEntity.java
@@ -7,8 +7,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -29,15 +27,13 @@ public class LivrosEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @NotBlank
+    
     private String nome;
 
-    @NotBlank
     private String editora;
-    @NotNull
+    
     private int anoPublicacao;
-
-    @NotBlank
+    
     private String genero;
 
     @ManyToOne
--- src/main/java/br/com/projetofaculdade/sistema_biblioteca/service/LivroService.java
+++ src/main/java/br/com/projetofaculdade/sistema_biblioteca/service/LivroService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import br.com.projetofaculdade.sistema_biblioteca.database.model.AutorEntity;
 import br.com.projetofaculdade.sistema_biblioteca.database.model.LivrosEntity;
+import br.com.projetofaculdade.sistema_biblioteca.database.repository.iAutorRepository;
 import br.com.projetofaculdade.sistema_biblioteca.database.repository.iLivroRepository;
 import br.com.projetofaculdade.sistema_biblioteca.dto.LivroDto;
 import br.com.projetofaculdade.sistema_biblioteca.exception.NotFoundException;
@@ -15,19 +17,26 @@ import lombok.RequiredArgsConstructor;
 public class LivroService {
 
     private final iLivroRepository livroRepository;
+    private final iAutorRepository autorRepository;
 
     public List<LivrosEntity> findAll(){
         return livroRepository.findAll();
     }
 
-    public void save(LivroDto livroDto) {
-        LivrosEntity livro = LivrosEntity.builder()
-            .nome(livroDto.getNome())
-            .editora(livroDto.getEditora())
-            .anoPublicacao(livroDto.getAnoPublicacao())
-            .genero(livroDto.getGenero())
-            .build();
+    public void save(LivroDto livroDto) throws NotFoundException {
+        AutorEntity autor = autorRepository.findById(livroDto.getAutorId())
+            .orElseThrow(() -> new NotFoundException("Autor não encontrado"));
+
+        LivrosEntity livro = new LivrosEntity();
+            livro.setNome(livroDto.getNome());
+            livro.setEditora(livroDto.getEditora());
+            livro.setAnoPublicacao(livroDto.getAnoPublicacao());
+            livro.setGenero(livroDto.getGenero());
+
+        livro.setAutor(autor);
+
         livroRepository.save(livro);
+            
     }
 
     public void update(Integer id, LivroDto livroDto) throws NotFoundException {
